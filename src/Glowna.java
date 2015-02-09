6a4be815efc74b88e3fde59cf163ee73b09a3e43
FireMorph <firemorph11@gmail.com>
2015-02-09 21:45:25 +0100

    Poczatek metody

--- src/Glowna.java
+++ src/Glowna.java
@@ -4,7 +4,26 @@
  */
 public class Glowna {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        int[] tablica = {1, 3, 5, 7, 9, 11};
+
+        for (int x : tablica)
+            System.out.print(x + " ");
+
+        new Glowna().metoda(tablica);
+
+        for (int x : tablica)
+            System.out.print(x + " ");
+    }
+
+    public int[] metoda(int[] tab) {
+        int[] temp = new int[tab.length];
+        int i = 0;
+
+        for (int x : tab)
+            temp[i++] = x;
+
+        return temp;
 
     }
 }
